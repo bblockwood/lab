d7a0c37619b5d64b1d2422a5587cb53dd6b25b8c
danlee22 <51303214+danlee22@users.noreply.github.com>
2019-06-05 14:44:46 -0400

    Code now imports from Dropbox
    installs packages, included comments, imports data from dropbox

--- projects/opportunity_atlas/code/IncTest.R
+++ projects/opportunity_atlas/code/IncTest.R
@@ -1,13 +1,19 @@
+install.packages("ggplot2")
+install.packages("dplyr")
+install.packages("tmap")
+install.packages("tmaptools")
+install.packages("sf")
+install.packages("leaflet")
 library(ggplot2)
 library(dplyr)
 library(tmap)
 library(tmaptools)
 library(sf)
 library(leaflet)
-tpdata <- read.csv("/Users/daniellee/Downloads/county_outcomes_simple.csv", header = T)
-tpdata <- tpdata[,c("czname", "kfr_pooled_pooled_p25")]
+tpdata <- read.csv("https://www.dropbox.com/s/xo4ddqzyherf44n/county_outcomes_simple.csv?dl=1", header = T) #imports Opportunity Atlas data from Dropbox
+tpdata <- tpdata[,c("czname", "kfr_pooled_pooled_p25")] #creates a table with only county names and kfr_pooled_pooled_p25
 print(tpdata)
-shapefile <- st_read("/Users/daniellee/Downloads/cb_2014_us_county_5m/cb_2014_us_county_5m.shp")
+shapefile <- st_read("https://www.dropbox.com/s/gyvsoi2tihqc1kj/cb_2014_us_county_5m.shp?dl=1") #imports us county shapefile from Dropbox
 usgeo$NAME <- as.character(usgeo$NAME)
 usgeo <- usgeo[order(usgeo$NAME),]
 tpdata <- tpdata[order(tpdata$czname),]
