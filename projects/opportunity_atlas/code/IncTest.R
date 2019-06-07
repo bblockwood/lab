1f1c30c4233e40cfc4a1a82be730cdc10d5f1cfa
danlee22 <51303214+danlee22@users.noreply.github.com>
2019-06-07 13:27:24 -0400

    Update IncTest.R
    Completely different way of mapping that was more successful
    many counties not filled in
    most probably due to incompatibility of us shapefile and data set, not code itself
    earlier iteration of code and simpler data set worked with California population density

--- projects/opportunity_atlas/code/IncTest.R
+++ projects/opportunity_atlas/code/IncTest.R
@@ -1,21 +1,43 @@
-install.packages("ggplot2")
-install.packages("dplyr")
-install.packages("tmap")
-install.packages("tmaptools")
-install.packages("sf")
-install.packages("leaflet")
 library(ggplot2)
+library(ggmap)
+library(maps)
+library(mapdata)
 library(dplyr)
-library(tmap)
-library(tmaptools)
-library(sf)
-library(leaflet)
-tpdata <- read.csv("https://www.dropbox.com/s/xo4ddqzyherf44n/county_outcomes_simple.csv?dl=1", header = T) #imports Opportunity Atlas data from Dropbox
-tpdata <- tpdata[,c("czname", "kfr_pooled_pooled_p25")] #creates a table with only county names and kfr_pooled_pooled_p25
-print(tpdata)
-shapefile <- st_read("https://www.dropbox.com/s/gyvsoi2tihqc1kj/cb_2014_us_county_5m.shp?dl=1") #imports us county shapefile from Dropbox
-usgeo$NAME <- as.character(usgeo$NAME)
-usgeo <- usgeo[order(usgeo$NAME),]
-tpdata <- tpdata[order(tpdata$czname),]
-usmap <- merge(usgeo, tpdata, by.x = "NAME", by.y = "czname")
-qtm(usmap, "kfr_pooled_pooled_p25")
+
+#map of US counties
+counties <- map_data("county")
+head(counties)
+us_base <- ggplot(data = counties, mapping = aes(x = long, y = lat, group = group)) + 
+  coord_fixed(1.3) + 
+  geom_polygon(color = "black", fill = "white")
+us_base
+
+#map of kfr_pooled_pooled_p25 (not sure what that is, but it's in the Opportunity Atlas data)
+IncData <- read.csv("https://www.dropbox.com/s/xo4ddqzyherf44n/county_outcomes_simple.csv?dl=1", header=T)
+IncData$outcome <- IncData$kfr_pooled_pooled_p25
+IncData$outcome <- IncData$outcome
+IncData$subregion <- tolower(IncData$subregion)
+IncData <- IncData[,c("subregion", "outcome")]
+head(IncData)
+names(IncData) <- c("subregion", "outcome")
+head(IncData)
+IncData$outcome <- as.numeric(as.character((IncData$outcome)))
+head(IncData)
+cacopa <- inner_join(counties, IncData, by = "subregion")
+head(cacopa)
+ditch_the_axes <- theme(
+  axis.text = element_blank(),
+  axis.line = element_blank(),
+  axis.ticks = element_blank(),
+  panel.border = element_blank(),
+  panel.grid = element_blank(),
+  axis.title = element_blank()
+)
+
+elbow_room1 <- us_base + 
+  geom_polygon(data = cacopa, aes(fill = outcome), color = "black") +
+  geom_polygon(color = "white", fill = NA) +
+  theme_bw() +
+  ditch_the_axes
+
+elbow_room1
