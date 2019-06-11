b306100119e775e4ae8dc040381e2ad0c6cc98b6
danlee22 <51303214+danlee22@users.noreply.github.com>
2019-06-11 12:18:01 -0400

    Opportunity Atlas Map
    #1 
    Used FIPS data and qtm

--- projects/opportunity_atlas/code/IncTest.R
+++ projects/opportunity_atlas/code/IncTest.R
@@ -1,43 +1,44 @@
+options(max.print=1000000)
 library(ggplot2)
-library(ggmap)
-library(maps)
-library(mapdata)
 library(dplyr)
-
+library(tmap)
+library(tmaptools)
+library(sf)
+library(leaflet)
 #map of US counties
 counties <- map_data("county")
-head(counties)
+View(as_data_frame(counties))
 us_base <- ggplot(data = counties, mapping = aes(x = long, y = lat, group = group)) + 
   coord_fixed(1.3) + 
   geom_polygon(color = "black", fill = "white")
 us_base
 
-#map of kfr_pooled_pooled_p25 (not sure what that is, but it's in the Opportunity Atlas data)
 IncData <- read.csv("https://www.dropbox.com/s/xo4ddqzyherf44n/county_outcomes_simple.csv?dl=1", header=T)
+View(as_data_frame(IncData))
+shapefile <- "/Users/daniellee/Downloads/USCounties/UScounties.shp"
+usageo <- read_shape(file=shapefile, as.sf = TRUE)
+View(as_data_frame(usageo))
 IncData$outcome <- IncData$kfr_pooled_pooled_p25
-IncData$outcome <- IncData$outcome
-IncData$subregion <- tolower(IncData$subregion)
-IncData <- IncData[,c("subregion", "outcome")]
+IncData$STATE_FIPS <- IncData$state
+IncData$STATE_FIPS <- as.numeric(as.character(IncData$STATE_FIPS))
+IncData$STATE_FIPS <- sprintf("%02d",IncData$STATE_FIPS)
+IncData$CNTY_FIPS <- IncData$county
+IncData$CNTY_FIPS <- as.numeric(as.character(IncData$CNTY_FIPS))
+head(IncData$CNTY_FIPS)
+IncData$CNTY_FIPS <- sprintf("%03d",IncData$CNTY_FIPS)
+head(IncData$CNTY_FIPS)
+IncData$FIPS <- paste(IncData$STATE_FIPS,IncData$CNTY_FIPS,sep="")
+head(IncData$FIPS)
+IncData <- IncData[,c("FIPS", "outcome")]
+View(as_data_frame(IncData))
 head(IncData)
-names(IncData) <- c("subregion", "outcome")
+names(IncData) <- c("FIPS", "outcome")
 head(IncData)
 IncData$outcome <- as.numeric(as.character((IncData$outcome)))
 head(IncData)
-cacopa <- inner_join(counties, IncData, by = "subregion")
+cacopa <- inner_join(usageo, IncData, by = NULL)
 head(cacopa)
-ditch_the_axes <- theme(
-  axis.text = element_blank(),
-  axis.line = element_blank(),
-  axis.ticks = element_blank(),
-  panel.border = element_blank(),
-  panel.grid = element_blank(),
-  axis.title = element_blank()
-)
+View(as_data_frame(cacopa))
 
-elbow_room1 <- us_base + 
-  geom_polygon(data = cacopa, aes(fill = outcome), color = "black") +
-  geom_polygon(color = "white", fill = NA) +
-  theme_bw() +
-  ditch_the_axes
+qtm(cacopa, "outcome")
 
-elbow_room1
