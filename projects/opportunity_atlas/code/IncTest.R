35b4ff4202876a4a335837c2b2e2b9cfb74f54c8
danlee22 <51303214+danlee22@users.noreply.github.com>
2019-06-17 11:03:43 -0400

    Unique User Directories Using Dropbox
    #1 
    change to user directory and it should be unique

--- projects/opportunity_atlas/code/IncTest.R
+++ projects/opportunity_atlas/code/IncTest.R
@@ -5,20 +5,21 @@ library(tmap)
 library(tmaptools)
 library(sf)
 library(leaflet)
-#map of US counties
-counties <- map_data("county")
-View(as_data_frame(counties))
-us_base <- ggplot(data = counties, mapping = aes(x = long, y = lat, group = group)) + 
-  coord_fixed(1.3) + 
-  geom_polygon(color = "black", fill = "white")
-us_base
 
-IncData <- read.csv("https://www.dropbox.com/s/xo4ddqzyherf44n/county_outcomes_simple.csv?dl=1", header=T)
-View(as_data_frame(IncData))
-shapefile <- "/Users/daniellee/Downloads/USCounties/UScounties.shp"
+#change to user directory
+if (Sys.info()[["user"]] == "daniellee") {
+  datapath <- "/Users/daniellee/Dropbox/RA_work/data/opportunity_atlas/input"
+}
+if (Sys.info()[["user"]] == "") {
+  datapath <- "/Users/bblockwood/Dropbox/RA_work/data/opportunity_atlas/input"
+}
+
+data <- paste(datapath,"/county_outcomes_simple.csv",sep="")
+IncData <- read.csv(data, header = T)
+shapefile <- paste(datapath,"/UScounties/UScounties.shp",sep="")
 usageo <- read_shape(file=shapefile, as.sf = TRUE)
-View(as_data_frame(usageo))
-IncData$outcome <- IncData$kfr_pooled_pooled_p25
+
+IncData$household_income_mean_percentile_rank <- IncData$kfr_pooled_pooled_p25
 IncData$STATE_FIPS <- IncData$state
 IncData$STATE_FIPS <- as.numeric(as.character(IncData$STATE_FIPS))
 IncData$STATE_FIPS <- sprintf("%02d",IncData$STATE_FIPS)
@@ -29,16 +30,15 @@ IncData$CNTY_FIPS <- sprintf("%03d",IncData$CNTY_FIPS)
 head(IncData$CNTY_FIPS)
 IncData$FIPS <- paste(IncData$STATE_FIPS,IncData$CNTY_FIPS,sep="")
 head(IncData$FIPS)
-IncData <- IncData[,c("FIPS", "outcome")]
-View(as_data_frame(IncData))
+IncData <- IncData[,c("FIPS", "household_income_mean_percentile_rank")]
 head(IncData)
-names(IncData) <- c("FIPS", "outcome")
+names(IncData) <- c("FIPS", "household_income_mean_percentile_rank")
 head(IncData)
-IncData$outcome <- as.numeric(as.character((IncData$outcome)))
+IncData$outcome <- as.numeric(as.character((IncData$household_income_mean_percentile_rank)))
 head(IncData)
 cacopa <- inner_join(usageo, IncData, by = NULL)
 head(cacopa)
 View(as_data_frame(cacopa))
-
-qtm(cacopa, "outcome")
+tmap_mode("view")
+qtm(cacopa, fill="household_income_mean_percentile_rank",fill.palette="RdBu")
 
